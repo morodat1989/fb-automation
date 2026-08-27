96f2556d8614458e3a168408b53467bdc1a6c3ed
Mr Dat Vsa <morodat1989@gmail.com>
2026-08-27 23:54:52 +0700

    Auto update: Thu 08/27/2026 23:54:52.15

--- utils/logger.py
+++ utils/logger.py
@@ -18,3 +18,20 @@ def log_success(message: str):
 
 def log_debug(message: str):
     print(f"[{_get_time()}] [DEBUG] {message}")
+
+# Class tương thích ngược cho các file gọi get_logger().info() / .error()
+class _LoggerAdapter:
+    def info(self, msg, *args, **kwargs):
+        log_info(str(msg))
+    def warning(self, msg, *args, **kwargs):
+        log_warning(str(msg))
+    def error(self, msg, *args, **kwargs):
+        log_error(str(msg))
+    def debug(self, msg, *args, **kwargs):
+        log_debug(str(msg))
+
+_global_logger = _LoggerAdapter()
+
+def get_logger(name: str = "FBAutomation"):
+    """Trả về đối tượng logger chuẩn tương thích với core/humanizer.py"""
+    return _global_logger
